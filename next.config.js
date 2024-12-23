f5e35d9c4bc8bcafd1f382d5ed31aca4b9c65c54
rfart <firdausamerta@gmail.com>
2024-12-23 19:01:16 +0700

    revert

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  experimental: {
-    appDir: false,
-  },
   images: {
     remotePatterns: [
       {
